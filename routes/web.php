be7784eb0ee9167b135aedc02eb0cc8053fca1bd
solomon3569 <silvermoon3569@hotmail.com>
2017-04-21 20:34:09 +0800

    註解

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts', function(){
+	return view('post');
+});
+
+Route::get('/posts/{id}', function($id){
+	return view('post-single',[
+		'id' => $id
+	]);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/user', 'UserController@index');
+Route::get('/user/delete/{id}', 'UserController@destroy');
+Route::post('/user', 'UserController@store');
